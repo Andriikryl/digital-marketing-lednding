35c6a4487d68e3463a50bb35949db01d5eb34a98
Andriikryl <andreyk557@gmail.com>
2023-05-09 23:30:19 +0300

    feat[burger-menu / mobile-header]

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-import './style.css'
+import "./style.css";
 
+const burger = document?.querySelector("[data-burger]");
+
+const nav = document?.querySelector("[data-nav]");
+const body = document.body;
+
+burger?.addEventListener("click", () => {
+  body.classList.toggle("stop-scroll");
+  burger?.classList.toggle("burger--active");
+  nav.classList.toggle("nav--visible");
+});
