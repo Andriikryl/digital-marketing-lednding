070693e24e75ef7484c4d72c4758b7b991bce292
Andriikryl <andreyk557@gmail.com>
2023-05-11 16:29:05 +0300

    feat[madia-styles.css]

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 import "./style.css";
 
 const burger = document?.querySelector("[data-burger]");
-
+const header = document.getElementById("page-header");
+const intercept = document.createElement("div");
 const nav = document?.querySelector("[data-nav]");
 const body = document.body;
 
+// Burger-menu
 burger?.addEventListener("click", () => {
   body.classList.toggle("stop-scroll");
   burger?.classList.toggle("burger--active");
   nav.classList.toggle("nav--visible");
 });
+
+// Burger-menu
+
+// Stycki-header
+
+intercept.setAttribute("data-observer-intercept", "");
+header.before(intercept);
+
+const observer = new IntersectionObserver(([entry]) => {
+  header.classList.toggle("active", !entry.isIntersecting);
+});
+
+observer.observe(intercept);
+
+// Stycki-header
